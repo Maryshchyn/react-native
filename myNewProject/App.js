a13dc429b25fadbf1e0ce9ceb9b0421459bcfe9a
Maryshchyn <maryshchyn89@gmail.com>
2023-01-04 16:24:51 +0200

    ук

--- myNewProject/App.js
+++ myNewProject/App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+} from "react-native";
 
 export default function App() {
+  const [value, setValue] = useState("");
+  const inputHandler = (text) => setValue(text);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <TextInput
+          placeholder="Type text"
+          value={value}
+          onChangeText={inputHandler}
+        />
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    
+    justifyContent: "flex-end",
+    paddingBottom: 30
   },
 });
